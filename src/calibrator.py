2db137c328cd8d138716bc0c3417717e4631d45c
CERLabRob <trejkev@gmail.com>
2022-10-18 01:21:42 -0600

    Working with full arena path follow

--- src/calibrator.py
+++ src/calibrator.py
@@ -4,33 +4,27 @@ __author__ = "Kevin Trejos Vargas"
 __email__  = "kevin.trejosvargas@ucr.ac.cr"
 
 # -- Imports -- #
-import rospy                                                                    # Needed for ros and python interaction
-import subprocess, time, os                                                     # Needed to interact with ros terminal
-
-from datetime        import datetime
-# from std_msgs.msg    import Float64                                             # Needed for APE publishing
-
+import rospy, subprocess, time, os                                              
+from datetime                 import datetime
 from slam_auto_calibrator.msg import APE
-# from MapAccuracy     import MapAccuracy     as MapMetric
-# from PoseAccuracy    import PoseAccuracy    as PoseMetric
+from MapAccuracy              import MapAccuracy as MapMetric
 # from ParamsGenerator import ParamsGenerator as Params
 
 rospy.init_node('slam_auto_calibrator') 
+# Params = Params()
 
-# MapMetric  = MapMetric(GroundTruthMap)
-# PoseMetric = PoseMetric()
-# Params     = Params()
-
-# message = APE()
 def APEReading(data):
     global RobotsNamespaceBase
-    rospy.loginfo("Actual frame id is " + data.frame_id)
+    global APETopicReadings
     if data.frame_id == RobotsNamespaceBase + "0":
-        pass
+        APETopicReadings[0] = data.translation_error_mean
+        APETopicReadings[0 + iRobotsQty] = data.rotation_error_mean
     elif data.frame_id == RobotsNamespaceBase + "1":
-        pass
+        APETopicReadings[1] = data.translation_error_mean
+        APETopicReadings[1 + iRobotsQty] = data.rotation_error_mean
     elif data.frame_id == RobotsNamespaceBase + "2":
-        pass
+        APETopicReadings[2] = data.translation_error_mean
+        APETopicReadings[2 + iRobotsQty] = data.rotation_error_mean
 
 ################################################################################
 ############################ -- ASSIGNING PARAMS -- ############################
@@ -46,8 +40,7 @@ sParamsDictName             = "Parameters_Robot"
 APETranslationTopicName     = "APETranslation"
 APERotationTopicName        = "APERotation"
 APETopicName                = "APE"
-APETranslationTopicReadings = list(range(3))
-APERotationTopicReadings    = list(range(3))
+APETopicReadings            = list(range(2*iRobotsQty))
 RobotsNamespaceBase         = "tb3_"
 dParams                     = {}                                                    # Stores the algorithm parameters
 
@@ -68,16 +61,20 @@ if rospy.has_param("/SLAMLaunchName"):
 # -- Parameters files
 if rospy.has_param("/RobotsQty"):
     sParamsDictName = rospy.get_param("/RobotsQty")
-# -- APE topics publishing translation and rotation APE mean
-if rospy.has_param("/APE_Translation_Topic_Name"):
-    APETranslationTopicName = rospy.get_param("/APE_Translation_Topic_Name")
-if rospy.has_param("/APE_Rotation_Topic_Name"):
-    APERotationTopicName = rospy.get_param("/APE_Rotation_Topic_Name")
+# -- APE topic publishing translation and rotation APE mean
 if rospy.has_param("/APE_Topic_Name"):
     APETopicName = rospy.get_param("/APE_Topic_Name")
 # -- Robots namespace base naming convention
 if rospy.has_param("/Robots_Namespace_Base"):
     RobotsNamespaceBase = rospy.get_param("/Robots_Namespace_Base")
+# -- Ground truth map file name
+if rospy.has_param("/Ground_Truth_Filename"):
+    GTName = rospy.get_param("/Ground_Truth_Filename")
+
+# -- Instance of the map error metric
+MapMetric = MapMetric()
+GTpath = os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/"
+MapMetric.setGroundTruthMap(GTpath + GTName)
 
 # -- Killing all gazebo processes that may be open
 subprocess.Popen("killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient", shell = True)
@@ -88,20 +85,16 @@ time.sleep(10)
 
 # -- Set the initial parameters
 # Params.setInitialParameters(dParameters = dParams)
-
-# -- Subscribe to each APE publisher robot topic
-APETranslationTopics = list(range(iRobotsQty))
-APERotationTopics    = list(range(iRobotsQty))
-for iRobot in range(iRobotsQty):
-    # APETranslationTopics[iRobot] = rospy.Subscriber("/" + RobotsNamespaceBase + str(iRobot) + "/" + APETranslationTopicName, APE, APEReading)
-    # APERotationTopics[iRobot]    = rospy.Subscriber("/" + RobotsNamespaceBase + str(iRobot) + "/" + APERotationTopicName   , APE, APEReading)
-    APERotationTopics[iRobot] = rospy.Subscriber("/" + RobotsNamespaceBase + str(iRobot) + "/" + APETopicName, APE, APEReading)
     
 
 for iActualCycle in range(iTrainingCycles):
     # -- Use each SLAM algorithm until they complete their route
     subprocess.Popen("roslaunch {} {}".format(sSLAMPackageName, sSLAMLaunchName), shell = True)
     time.sleep(10)
+    # -- Subscribe to each APE publisher robot topic
+    APETopics = list(range(iRobotsQty))
+    for iRobot in range(iRobotsQty):
+        APETopics[iRobot] = rospy.Subscriber("/" + RobotsNamespaceBase + str(iRobot) + "/" + APETopicName, APE, APEReading)
     bRunCompleted = False
     while bRunCompleted == False:
         nodes = os.popen("rosnode list").readlines()
@@ -114,15 +107,25 @@ for iActualCycle in range(iTrainingCycles):
                 break
             if iNodeCount == len(nodes):
                 bRunCompleted = True
-        time.sleep(10)
+        time.sleep(5)
             
     # -- Generate the info needed by map metric
     date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
     MapName = "{sn}_Trial_{t}_RobotsQty_{rc}_Map_{d}".format(sn = sSLAMName, t = iActualCycle, rc = iRobotsQty, d = date)
-    subprocess.Popen("rosrun map_server map_saver -f ~/catkin_ws/src/slam_auto_calibrator/Maps/{}".format(MapName), shell = True)
+    subprocess.Popen("rosrun map_server map_saver --occ 90 --free 10 -f ~/catkin_ws/src/slam_auto_calibrator/Maps/{}".format(MapName), shell = True)
     time.sleep(2)
-    # -- Generate the info needed by pose metric
 
+    # -- Compute the metrics
+    rospy.loginfo("Map path is: '" + os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/{}.pgm".format(MapName) + "'")
+    fActualMapError  = MapMetric.compute(os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/{}.pgm".format(MapName))
+    rospy.loginfo("Map error for cycle {} is: {}".format(iActualCycle, fActualMapError))
+    rospy.loginfo("Robot 0 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[0]))
+    rospy.loginfo("Robot 0 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[0 + iRobotsQty]))
+    rospy.loginfo("Robot 1 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[1]))
+    rospy.loginfo("Robot 1 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[1 + iRobotsQty]))
+    rospy.loginfo("Robot 2 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[2]))
+    rospy.loginfo("Robot 2 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[2 + iRobotsQty]))
+    
     # -- Kill the SLAM algorithms and their related nodes
     nodes = os.popen("rosnode list").readlines()
     for index in range(len(nodes)):
@@ -133,12 +136,6 @@ for iActualCycle in range(iTrainingCycles):
             or 'map_saver'       in node \
             or 'turtlebot3_slam' in node:
             os.system("rosnode kill " + node)
-    # -- Compute the metrics
-    # fActualMapError  = MapMetric.compute(ActualMap)
-    # fActualXPoseError, fActualYPoseError, fActualRotPoseError = PoseMetric.compute(GroundTruthPose, ActualPose)
-    # if iActualCycle == 0:
-    #     log.debug("Initial map error is {} meters".format(fActualMapError))
-    #     log.debug("Initial pose error is {} meters".format(fActualPoseError))
     # -- Feed the AI to generate the new set of parameters
     # dParams = Params.computeNewParameters(MapError     = fActualMapError    ,
     #                                       XPoseError   = fActualXPoseError  ,
@@ -146,8 +143,8 @@ for iActualCycle in range(iTrainingCycles):
     #                                       RotPoseError = fActualRotPoseError)
     # -- Modify the SLAM parameters into their launch files
 
-# log.debug("Final map error is {} meters".format(fActualMapError))
-# log.debug("Final pose error is {} meters".format(fActualPoseError))
+# rospy.loginfo("Final map error is {} meters".format(fActualMapError))
+# rospy.loginfo("Final pose error is {} meters".format(fActualPoseError))
 # -- Kill the gazebo related nodes
 nodes = os.popen("rosnode list").readlines()
 for index in range(len(nodes)):
