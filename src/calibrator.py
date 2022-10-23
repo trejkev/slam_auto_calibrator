7ae89f61ad88230fd1269fb708a55561376f4c48
CERLabRob <trejkev@gmail.com>
2022-10-23 00:14:41 -0600

    Increased wait times

--- src/calibrator.py
+++ src/calibrator.py
@@ -7,8 +7,7 @@ __email__  = "kevin.trejosvargas@ucr.ac.cr"
 import rospy, subprocess, time, os                                              
 from datetime                 import datetime
 from slam_auto_calibrator.msg import APE
-from MapAccuracy              import MapAccuracy as MapMetric
-from ParamsGenerator import ParamsGenerator as Params
+from ParamsGenerator          import ParamsGenerator as Params
 
 rospy.init_node('slam_auto_calibrator') 
 
@@ -24,6 +23,22 @@ def APEReading(data):
     elif data.frame_id == RobotsNamespaceBase + "2":
         APETopicReadings[2] = data.translation_error_mean
         APETopicReadings[2 + iRobotsQty] = data.rotation_error_mean
+    
+################################################################################
+################################## CLEAN START #################################
+################################################################################
+# -- Killing all gazebo processes that may be open
+subprocess.Popen("killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient", shell = True)
+time.sleep(10)
+# -- Kill all nodes for a clean start
+nodes = os.popen("rosnode list").readlines()
+for index in range(len(nodes)):
+    nodes[index] = nodes[index].replace("\n", "")
+for node in nodes:
+    if 'rosout' not in node and 'slam_auto_calibrator' not in node:
+        os.system("rosnode kill " + node)
+time.sleep(10)
+
 
 ################################################################################
 ############################ -- ASSIGNING PARAMS -- ############################
@@ -73,30 +88,29 @@ if rospy.has_param("/Ground_Truth_Filename"):
 if rospy.has_param("/Params_File_Path"):
     sParamsFilePath = rospy.get_param("/Params_File_Path")
 
-# -- Instance of the map error metric
-MapMetric = MapMetric()
+# -- Path to map error metric
 GTpath = os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/"
-MapMetric.setGroundTruthMap(GTpath + GTName)
+GTMapPath = GTpath + GTName
 
-# -- Killing all gazebo processes that may be open
-subprocess.Popen("killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient", shell = True)
-time.sleep(2)
 # -- Open the arena with three robots
 subprocess.Popen("roslaunch slam_auto_calibrator {lf}".format(lf = sRobotsLauchName), shell = True)
-time.sleep(10)
+time.sleep(60)
 
 # -- Call the parameters generator
 Params = Params(sParamsFilePath)
-    
 
 for iActualCycle in range(iTrainingCycles):
-    # -- Use each SLAM algorithm until they complete their route
+    
+    # -- Launch the SLAM algorithms and the automatic navigator
     subprocess.Popen("roslaunch {} {}".format(sSLAMPackageName, sSLAMLaunchName), shell = True)
     time.sleep(10)
+    
     # -- Subscribe to each APE publisher robot topic
     APETopics = list(range(iRobotsQty))
     for iRobot in range(iRobotsQty):
         APETopics[iRobot] = rospy.Subscriber("/" + RobotsNamespaceBase + str(iRobot) + "/" + APETopicName, APE, APEReading)
+    
+    # --- Wait for the robots to complete their travel
     bRunCompleted = False
     while bRunCompleted == False:
         nodes = os.popen("rosnode list").readlines()
@@ -112,22 +126,34 @@ for iActualCycle in range(iTrainingCycles):
         time.sleep(5)
             
     # -- Generate the info needed by map metric
-    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p").replace(":","_")
     MapName = "{sn}_Trial_{t}_RobotsQty_{rc}_Map_{d}".format(sn = sSLAMName, t = iActualCycle, rc = iRobotsQty, d = date)
-    subprocess.Popen("rosrun map_server map_saver --occ 90 --free 10 -f ~/catkin_ws/src/slam_auto_calibrator/Maps/{}".format(MapName), shell = True)
+    subprocess.Popen("rosrun map_server map_saver -f ~/catkin_ws/src/slam_auto_calibrator/Maps/{}".format(MapName), shell = True)
     time.sleep(2)
 
-    # -- Compute the metrics
+    # -- Compute the map metric
     rospy.loginfo("Map path is: '" + os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/{}.pgm".format(MapName) + "'")
-    fActualMapError  = MapMetric.compute(os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/{}.pgm".format(MapName))
+    sSLAMMapPath = os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/Maps/{}.pgm".format(MapName)
+    with open(os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/src/MapMetricVariables.txt", "w") as mmv:
+        mmv.write("GTMapPath={}\n".format(GTMapPath))
+        mmv.write("SLAMMapPath={}\n".format(sSLAMMapPath))
+        mmv.close()
+    MapMetricFile = os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/src/MapAccuracy.py"
+    process = subprocess.Popen("'{}'".format(MapMetricFile), shell = True)
+    process.wait()
+    with open(os.getcwd().replace('.ros','') + "catkin_ws/src/slam_auto_calibrator/src/MapMetricVariables.txt", "r") as mmv:
+        for line in mmv.readlines():
+            fActualMapError = float(line.split("=")[1])
+    
     rospy.loginfo("Map error for cycle {} is: {}".format(iActualCycle, fActualMapError))
     rospy.loginfo("Robot 0 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[0]))
-    rospy.loginfo("Robot 0 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[0 + iRobotsQty]))
+    rospy.loginfo("Robot 0 rotation    error for cycle {} is: {}".format(iActualCycle, APETopicReadings[0 + iRobotsQty]))
     rospy.loginfo("Robot 1 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[1]))
-    rospy.loginfo("Robot 1 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[1 + iRobotsQty]))
+    rospy.loginfo("Robot 1 rotation    error for cycle {} is: {}".format(iActualCycle, APETopicReadings[1 + iRobotsQty]))
     rospy.loginfo("Robot 2 translation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[2]))
-    rospy.loginfo("Robot 2 rotation error for cycle {} is: {}".format(iActualCycle, APETopicReadings[2 + iRobotsQty]))
-    Params.computeNewParameters(iMapError = fActualMapError , lPoseError = APETopicReadings)
+    rospy.loginfo("Robot 2 rotation    error for cycle {} is: {}".format(iActualCycle, APETopicReadings[2 + iRobotsQty]))
+    
+    Params.computeNewParameters(iMapError = fActualMapError , lPoseError = APETopicReadings)                            # Feeding features to ML algorithm to generate new params
     
     # -- Kill the SLAM algorithms and their related nodes
     nodes = os.popen("rosnode list").readlines()
@@ -137,17 +163,11 @@ for iActualCycle in range(iTrainingCycles):
         if 'rviz'                in node \
             or 'map_merge'       in node \
             or 'map_saver'       in node \
-            or 'turtlebot3_slam' in node:
+            or 'turtlebot3_slam' in node \
+            or 'APE'             in node:
             os.system("rosnode kill " + node)
-    # -- Feed the AI to generate the new set of parameters
-    # dParams = Params.computeNewParameters(MapError     = fActualMapError    ,
-    #                                       XPoseError   = fActualXPoseError  ,
-    #                                       YPoseError   = fActualYPoseError  ,
-    #                                       RotPoseError = fActualRotPoseError)
-    # -- Modify the SLAM parameters into their launch files
-
-# rospy.loginfo("Final map error is {} meters".format(fActualMapError))
-# rospy.loginfo("Final pose error is {} meters".format(fActualPoseError))
+            rospy.loginfo("Cycle {} completed: rosnode kill {}".format(iActualCycle, node))
+            
 # -- Kill the gazebo related nodes
 nodes = os.popen("rosnode list").readlines()
 for index in range(len(nodes)):
@@ -155,4 +175,6 @@ for index in range(len(nodes)):
 for node in nodes:
     if 'rosout' not in node and 'slam_auto_calibrator' not in node:
         os.system("rosnode kill " + node)
+        rospy.loginfo("All cycles completed: rosnode kill {}".format(node))
 subprocess.Popen("killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient", shell = True)
+os.system("rosnode kill slam_auto_calibrator")
